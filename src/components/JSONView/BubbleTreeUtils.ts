ce2ea819ff6669d4ea4f98394e9d9ed763ab044d
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2023-10-25 16:16:23 +0530

    added horizontal link tree using d3

---
+++ src/components/JSONView/BubbleTreeUtils.ts
@@ -0,0 +1,45 @@
+export function ToJsonTree(json, rootName) {
+
+
+
+  let JsonTree = {
+    "name": rootName,
+    "value": "children count: " + Object.keys(json).length,
+    "children": [],
+  };
+
+  for (let key in json) {
+
+    const currentObj = {
+      "name": key,
+    };
+
+    // nested nodes if inner element is an array
+    if (json[key] instanceof Array) {
+
+      currentObj.value = "children count: " + json[key].length;
+      currentObj.children = json[key].map((item, index) => {
+        let obj = { "name": `${key}[${index}]` };
+        if (item instanceof Object)
+          obj.value = JSON.stringify(item);
+        else
+          obj.value = item;
+        return obj;
+      });
+
+      JsonTree.children.push(currentObj);
+    }
+
+    // nested nodes if inner element is JSON itself
+    else if (json[key] instanceof Object) {
+      JsonTree.children.push(ToJsonTree(json[key], key));
+    }
+
+    else {
+      currentObj.value = json[key];
+      JsonTree.children.push(currentObj);
+    }
+  }
+
+  return JsonTree;
+}
--- src/components/JSONView/BubbleViewer.tsx
+++ src/components/JSONView/BubbleViewer.tsx
@@ -1,18 +1,47 @@
-import { isObject } from '../homepage/homepageUtils'
+import { useEffect } from 'react'
+import { select, hierarchy, tree, linkHorizontal, json, linkVertical, linkRadial } from 'd3'
+import { ToJsonTree } from './BubbleTreeUtils';
 
 const BubbleViewer = ({ data }: { data: any }) => {
-  if (isObject(data)) {
-    return Object.keys(data).map((instance) => {
-      return <BubbleViewer data={instance} />
-    })
-  }
-  if (Array.isArray(data)) {
-    return data.map((instance) => {
-      return <BubbleViewer data={instance} />
-    })
-  }
+  const width = document.body.clientWidth;
+  const height = document.body.clientHeight;
+  const treeLayout = tree().size([height, width])
+  useEffect(() => {
+    const svg = select(document.getElementById("treeViewSvg"))
+    svg.selectAll('*').remove()
 
-  return <div>{JSON.stringify(data)}</div>
+    svg.attr('width', width).attr('height', height)
+
+
+    const root = hierarchy(ToJsonTree(data, 'glossary'))
+    const paths = treeLayout(root).links();
+    const pathGenerator = linkHorizontal().x(d => d.y).y(d => d.x)
+    console.log(root, paths)
+    svg.selectAll('path')
+      .data(paths)
+      .enter()
+      .append('path')
+      .attr('fill', 'none')
+      .attr('stroke', '#000')
+      .attr('stroke-width', 2)
+      .attr('d', pathGenerator)
+
+    svg.selectAll('text')
+      .data(root.descendants())
+      .enter()
+      .append('text')
+      .attr('color', '#000')
+      .attr('font-size', '0.75rem')
+      .attr('x', d => d.y)
+      .attr('y', d => d.x)
+      .text(({ data }) => data.name)
+
+  }, [])
+
+  return <div>
+    <svg id="treeViewSvg"></svg>
+  </div>
 }
 
 export default BubbleViewer
+
